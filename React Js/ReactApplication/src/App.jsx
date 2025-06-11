263219c98e8b36873885fc51346fc59670a0899e
jsureshbsc <jsureshj2002@gmail.com>
2025-06-11 22:17:30 +0530

    suresh

--- React Js/ReactApplication/src/App.jsx
+++ React Js/ReactApplication/src/App.jsx
@@ -1,44 +1,22 @@
 // import './App.css'
-
 import Header from "./components/Header";
 import Content from "./components/Content";
 import Footer from "./components/Footer";
-import { createContext, useContext, useState } from "react";
-import Header from './components/Header';
-import Content from './components/Content';
-import Footer from './components/Footer';
-import ChangingName from './components/ChangingName';
-
-
+import ChangingName from "./components/ChangingName";
 
 function App() {
-  let [user, setUser] = useState({
-    name: "suresh",
-    age: 23,
-    email: "suresh@gmail.com",
-  });
+  let userName = "suresh";
+
   return (
     <>
-      <userContext.Provider value={{ user }}>
-        <div className="app">
-          <Header />
-          <Content />
-          <Footer />
-        </div>
-      </userContext.Provider>
+      <div className="app">
+        <Header userName={userName} />
+        <Content />
+        <ChangingName />
+        <Footer />
+      </div>
     </>
   );
-   <>
-    
-    <div className='app'>
-    <Header userName={userName}/>
-    <Content/>
-    <ChangingName/>
-    <Footer/>
-    </div>
-   </>
-  )
 }
 
-export let userContext = createContext();
 export default App;
